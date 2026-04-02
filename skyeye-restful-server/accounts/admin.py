fde508d473064afff413da69f3ee7e4dfe8cbe60
yangyang1598 <0815yjh@gmail.com>
2026-04-02 23:40:15 +0000

    그룹 자동 선택 및 User 추가 창 구성 변경경

--- skyeye-restful-server/accounts/admin.py
+++ skyeye-restful-server/accounts/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from accounts.models import *
 from django.contrib.admin.models import LogEntry
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.models import Group
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+from accounts.models import NotificationUser, User
+
+
+class AccountCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ("username", "email", "password1", "password2", "groups", "is_staff", "is_active", "is_superuser")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        manage_group = Group.objects.filter(name="Manage").first()
+        if manage_group and "groups" in self.fields:
+            self.fields["groups"].initial = [manage_group.pk]
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+        if commit:
+            manage_group = Group.objects.filter(name="Manage").first()
+            if manage_group:
+                user.groups.add(manage_group)
+        return user
+
+
+class AccountChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+        fields = "__all__"
+
 
 class AccountAdmin(UserAdmin):
     # 관리자 화면에 보여질 칼럼 지정
@@ -9,9 +41,33 @@ class AccountAdmin(UserAdmin):
     search_fields = ('email', 'username')
     readonly_fields = ('id', 'last_login')
 
-    filter_horizontal = ()
+    add_form = AccountCreationForm
+    form = AccountChangeForm
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2", "is_superuser", "groups", "is_staff", "is_active"),
+            },
+        ),
+    )
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("is_superuser", "email", "groups", "is_staff", "is_active"),
+            },
+        ),
+    )
+
+    filter_horizontal = ("groups")
     list_filter = ()
-    fieldsets = ()
+
+    def response_add(self, request, obj, post_url_continue=None):
+        return HttpResponseRedirect(reverse("admin:accounts_user_changelist"))
 
 
 class NotificationAdmin(admin.ModelAdmin):
