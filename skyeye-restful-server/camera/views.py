0108d954c994f260dab8a9fd2cd3ae29ac5ac49d
yangyang1598 <0815yjh@gmail.com>
2026-04-09 01:29:40 +0000

    camera_view siteid 20초 차이 갱신

--- skyeye-restful-server/camera/views.py
+++ skyeye-restful-server/camera/views.py
@@ -8,6 +8,7 @@ from datetime import datetime, timedelta
 from django.utils.timezone import now
 from django.db.models import Q
 import logging
+from django.utils import timezone
 
 db_logger = logging.getLogger('db')
 
@@ -55,7 +56,7 @@ class CameraViewViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-     # 최근 20초이내 최신 데이터를 반환
+    # 최근 20초이내 최신 데이터를 반환
     def list(self, request, *args, **kwargs):
         try:
             site = request.query_params.get('site')
@@ -67,9 +68,22 @@ class CameraViewViewSet(viewsets.ModelViewSet):
                 )
             
             site = int(site)
-            threshold_time = now() - timedelta(seconds=20)
 
-            latest_data = (self.queryset.filter(site_id=site, date__gte=threshold_time).order_by('-date').first())
+            # 현재 서버의 KST(한국시간)를 가져옵니다. (예: 10시 05분 49초)
+            local_time = timezone.localtime()
+            
+            # DB(외부 장비 등)에는 KST 시간이 timezone 없이 저장되지만, 
+            # Django(USE_TZ=True)는 DB의 시간을 UTC로 취급하여 9시간의 오차가 발생하고 있었습니다.
+            # 이를 해결하기 위해 KST 시간의 시/분/초 그대로를 UTC 객체로 덮어은워 쿼리에 사용합니다.
+            db_current_time = local_time.replace(tzinfo=timezone.utc)
+            db_threshold_time = db_current_time - timedelta(seconds=20)
+
+            # 정확히 최근 20초 이내의 KST 시간에 생성된 데이터만 필터링
+            latest_data = (self.queryset.filter(
+                site_id=site, 
+                date__gte=db_threshold_time,
+                date__lte=db_current_time
+            ).order_by('-date').first())
             if not latest_data:
                 return Response({"message": "No data found"}, status=status.HTTP_204_NO_CONTENT)
 
